af0db7ccd428f8170ad1614d09d9584b746fd255
Lukasz Metrak <Lukasz.Metrak@fisglobal.com>
2024-03-05 23:42:55 +0100

    removing unnecessary imports

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,11 +2,6 @@ package com.example.demo;
 
 import jakarta.servlet.ServletRegistration;
 import org.eclipse.jetty.ee10.servlet.DefaultServlet;
-import org.eclipse.jetty.server.Connector;
-import org.eclipse.jetty.server.ServerConnector;
-import org.eclipse.jetty.server.SymlinkAllowedResourceAliasChecker;
-import org.eclipse.jetty.server.handler.ContextHandler;
-import org.eclipse.jetty.util.resource.MountedPathResource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -14,14 +9,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
 import java.net.URL;
 
 @SpringBootApplication
 public class DemoApplication implements WebServerFactoryCustomizer<JettyServletWebServerFactory> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DemoApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
     @Override
     public void customize(JettyServletWebServerFactory factory) {
@@ -41,9 +38,5 @@ public class DemoApplication implements WebServerFactoryCustomizer<JettyServletW
         });
         LOGGER.info("WebServer customized with swagger-ui");
 
-    }
-
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
     }
 }
