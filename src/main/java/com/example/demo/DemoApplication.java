95b839a7748616ead697ddc675dcdc17b9bf3a0e
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2024-03-06 20:14:10 -0600

    Update from @lachlan-roberts

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,8 +1,13 @@
 package com.example.demo;
 
+import java.io.IOException;
+import java.net.URL;
+
 import org.eclipse.jetty.ee10.servlet.DefaultServlet;
 import org.eclipse.jetty.ee10.servlet.ServletContextHandler;
 import org.eclipse.jetty.ee10.servlet.ServletHolder;
+import org.eclipse.jetty.server.AllowedResourceAliasChecker;
+import org.eclipse.jetty.util.resource.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -10,9 +15,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.core.io.ClassPathResource;
-
-import java.io.IOException;
-import java.net.URL;
 
 @SpringBootApplication
 public class DemoApplication implements WebServerFactoryCustomizer<JettyServletWebServerFactory> {
@@ -23,24 +25,23 @@ public class DemoApplication implements WebServerFactoryCustomizer<JettyServletW
         SpringApplication.run(DemoApplication.class, args);
     }
 
-@Override
-public void customize(JettyServletWebServerFactory factory) {
-    factory.addInitializers(ctx -> {
-        ServletContextHandler servletContextHandler = ServletContextHandler.getServletContextHandler(ctx);
-//        servletContextHandler.clearAliasChecks();
-//        servletContextHandler.addAliasCheck((pathInContext, resource)-> true); // disable alias checks by returning true always
+    @Override
+    public void customize(JettyServletWebServerFactory factory) {
+        factory.addInitializers(ctx -> {
+            ServletContextHandler servletContextHandler = ServletContextHandler.getServletContextHandler(ctx);
+            ServletHolder holder = new ServletHolder("swatter-ui", new DefaultServlet());
+            servletContextHandler.addServlet(holder, "/swagger-ui/*");
 
-        ServletHolder holder = new ServletHolder("swatter-ui", new DefaultServlet());
-        servletContextHandler.addServlet(holder, "/swagger-ui/*");
-
-        try {
-            URL externalForm = new ClassPathResource("/static/swagger-ui/").getURL();
-            LOGGER.info("### resources: " + externalForm.toExternalForm());
-            holder.setInitParameter("baseResource", externalForm.toExternalForm());
-        } catch (IOException e) {
-            throw new RuntimeException("Unable to find /static/swagger-ui");
-        }
-    });
-    LOGGER.info("WebServer customized with swagger-ui");
-}
+            try {
+                URL externalForm = new ClassPathResource("/static/swagger-ui/").getURL();
+                LOGGER.info("### resources: " + externalForm.toExternalForm());
+                holder.setInitParameter("baseResource", externalForm.toExternalForm());
+                Resource base = servletContextHandler.newResource(externalForm);
+                servletContextHandler.addAliasCheck(new AllowedResourceAliasChecker(servletContextHandler, base));
+            } catch (IOException e) {
+                throw new RuntimeException("Unable to find /static/swagger-ui");
+            }
+        });
+        LOGGER.info("WebServer customized with swagger-ui");
+    }
 }
